5cc9bc2d7bf59e8a815b7ac8ff5f60ea49fd8ebf
Kattebu <butskoev@tut.by>
2023-03-11 22:24:11 +0300

    Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Задача 64: Задайте значение N. Напишите программу, 
 // которая выведет все натуральные числа в промежутке от N до 1. 
 // Выполнить с помощью рекурсии.
+
+Console.WriteLine("введите число N:");
+int N=Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return(start+","+PrintNumbers(start-1,end));
+}
+Console.WriteLine(PrintNumbers(N,1));
